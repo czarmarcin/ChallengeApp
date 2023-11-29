ef35d799c1f26ad76bec7c4e6cacc233f5c9043c
czarmarcin <czar.marcin@gmail.com>
2023-11-29 13:01:29 +0100

    zadanie domowe Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,40 +1,21 @@
 ﻿//zadanie domowe Dzień 4
-using System.ComponentModel.Design;
-
 var name = "Ewa";
-char gender = 'M';
-var age = 120;
-//dodatkowe - weryfikacja wieku
-if (age < 18)
-{
-    Console.WriteLine("Dostęp tylko dla osób pełnoletnich !");
-}
-else
-{
-    Console.WriteLine("Zapraszamy");
-}
-//część główna zadania
+char gender = 'K';
+var age = 33;
+
 if (gender == 'K' && age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
-if (name == "Ewa" && age == 33 )
+else if (name == "Ewa" && gender =='K' && age == 33)
 {
     Console.WriteLine("Ewa, lat 33");
 }
-if (gender == 'M' && age < 18)
+else if (gender == 'M' && age < 18)
 {
     Console.WriteLine("Niepełnoletni mężczyzna");
 }
-else if (gender == 'M' && age < 60) 
+else  
 {
-    Console.WriteLine("Dojrzały mężczyzna");
-}
-else if (gender == 'M' && age < 120) 
-{
-    Console.WriteLine("Starszy mężczyzna");
-}
-else if (gender == 'M' && age >= 120) 
-{
-    Console.WriteLine("Nie ma takiej opcji");
+    Console.WriteLine("Nie wiadomo kim jesteś");
 }
