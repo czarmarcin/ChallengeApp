a7b6cc11c93864d3feb56aa0fb9ac1b41b8f2d9b
czarmarcin <czar.marcin@gmail.com>
2023-11-29 20:22:43 +0100

    zadanie domowe Dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,21 +1,58 @@
-﻿//zadanie domowe Dzień 4
-var name = "Ewa";
-char gender = 'K';
-var age = 33;
+﻿//zadanie domowe Dzień 5
+int number = 345677;
+string numberAsString = number.ToString();
+List<char> digits = numberAsString.ToList();
 
-if (gender == 'K' && age < 30)
+int[] counters = new int[10];
+for (int i = 0; i < counters.Length; i++)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    counters[i] = 0;
 }
-else if (name == "Ewa" && gender =='K' && age == 33)
+
+foreach (char digit in digits)
 {
-    Console.WriteLine("Ewa, lat 33");
+    if (digit == '0')
+    {
+        counters[0]++;
+    }
+    else if (digit == '1')
+    {
+        counters[1]++;
+    }
+    else if (digit == '2')
+    {
+        counters[2]++;
+    }
+    else if (digit == '3')
+    {
+        counters[3]++;
+    }
+    else if (digit == '4')
+    {
+        counters[4]++;
+    }
+    else if (digit == '5')
+    {
+        counters[5]++;
+    }
+    else if (digit == '6')
+    {
+        counters[6]++;
+    }
+    else if (digit == '7')
+    {
+        counters[7]++;
+    }
+    else if (digit == '8')
+    {
+        counters[8]++;
+    }
+    else if (digit == '9')
+    {
+        counters[9]++;
+    }
 }
-else if (gender == 'M' && age < 18)
+for (int i = 0; i < counters.Length; i++)
 {
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
-else  
-{
-    Console.WriteLine("Nie wiadomo kim jesteś");
+    Console.WriteLine("Cyfra " + i + " występuje " + counters[i] + " razy");
 }
