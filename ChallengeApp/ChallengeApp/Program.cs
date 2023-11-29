ed23fee60840b5f73781be70dcea8ee69ea43163
czarmarcin <czar.marcin@gmail.com>
2023-11-29 10:33:33 +0100

    zadanie domowe Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//zadanie domowe Dzień 4
+var name = "Ewa";
+char gender = 'K';
+var age = 33;
+//dodatkowe - weryfikacja wieku
+if (age < 18)
+{
+    Console.WriteLine("Dostęp tylko dla osób pełnoletnich !");
+}
+else
+{
+    Console.WriteLine("Zapraszamy");
+}
+//część główna zadania
+if (gender == 'K' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+if (name == "Ewa" && age == 33 )
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+if (gender == 'M' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
