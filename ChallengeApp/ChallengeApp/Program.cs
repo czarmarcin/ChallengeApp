87898dfd440c1df591e2996e6a585047259748a3
czarmarcin <czar.marcin@gmail.com>
2023-12-07 17:30:16 +0100

    dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,26 +1,84 @@
-﻿//zadanie domowe Dzień 5
-int number = 345677251;
-string numberAsString = number.ToString();
-List<char> digits = numberAsString.ToList();
-char[] checkForCounters = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-int[] counters = new int[10];
-for (int i = 0; i < counters.Length; i++)
-{
-    counters[i] = 0;
-}
+﻿//zadanie domowe Dzień 6
 
-foreach (char digit in digits)
+Empl empl1 = new Empl("Adam", "Słodowy", 44);
+Empl empl2 = new Empl("Roman", "Romanowski", 18);    
+Empl empl3 = new Empl("Zenobi", "vel Golota",99);
+
+empl1.AddScore(1);
+empl1.AddScore(5);  
+empl1.AddScore(1);
+
+empl2.AddScore(1);
+empl2.AddScore(5);
+empl2.AddScore(1);
+
+empl3.AddScore(1);
+empl3.AddScore(5);
+empl3.AddScore(1);
+
+if (empl1.result >= empl2.result && empl1.result >= empl3.result)
 {
-    for (int j = 0; j < checkForCounters.Length; j++)
+    int highResult = empl1.result;
+    Console.WriteLine("Najwięcej punktów zdobył " + empl1.name + " " + empl1.surname + "-" + highResult);
+    if (empl1.result == empl2.result)
     {
-        if (digit == checkForCounters[j])
-        {
-            counters[j]++;
-        }
+        Console.WriteLine("Tyle samo zdobył " + empl2.name + " " + empl2.surname);
+    }
+    if (empl1.result == empl3.result)
+    {
+        Console.WriteLine("Tyle samo zdobył " + empl3.name + " " + empl3.surname);
+    }
+
+}
+else if (empl2.result >= empl1.result && empl2.result >= empl3.result)
+{
+    int highResult = empl2.result;
+    Console.WriteLine("Najwięcej punktów zdobył " + empl2.name + " " + empl2.surname + "-" + highResult);
+    if (empl2.result == empl1.result)
+    {
+        Console.WriteLine("Tyle samo zdobył " + empl1.name + " " + empl1.surname);
+    }
+    if (empl2.result == empl3.result)
+    {
+        Console.WriteLine("Tyle samo zdobył " + empl3.name + " " + empl3.surname);
     }
 }
-Console.WriteLine("W liczbie : "+number);
-for (int i = 0; i < counters.Length; i++)
+else if (empl3.result >= empl1.result && empl3.result >= empl2.result)
 {
-    Console.WriteLine("cyfra " + i + " występuje " + counters[i] + " razy");
+    int highResult = empl3.result;
+    Console.WriteLine("Najwięcej punktów zdobył " + empl3.name + " " + empl3.surname + "-" + highResult);
+    if (empl3.result == empl1.result)
+    {
+        Console.WriteLine("Tyle samo zdobył " + empl1.name + " " + empl1.surname);
+    }
+    if (empl3.result == empl2.result)
+    {
+        Console.WriteLine("Tyle samo zdobył " + empl2.name + " " + empl2.surname);
+    }
+
+}
+class Empl
+{
+    private List<int> score = new List<int>();
+    public Empl(string name, string surname, int age)
+    {
+        this.name = name ;
+        this.surname = surname ;    
+        this.Age = age ;
+    }
+    public string name { get; private set; }
+    public string surname { get; private set; } 
+    public int Age { get; private set;}
+
+    public void AddScore(int number)
+    {
+        this.score.Add(number);
+    }
+    public int result
+    {
+        get
+        { 
+            return this.score.Sum(); 
+        }
+    }
 }
